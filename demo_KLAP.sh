5017d45bca210bb2bc072f7c493316f76c3a6be0
Wongi Jeong <87856112+ignoww@users.noreply.github.com>
2023-08-13 17:24:56 +0900

    Update demo_KLAP.sh

--- demo_KLAP.sh
+++ demo_KLAP.sh
@@ -1,1 +1,1 @@
-python test_KLAP_DF2K.py  --save-dir "./experiments/KLAP" --gpu_num 0  --mode quad  --pre_train "PRETRAINED_WEIGHT_PATH" --lr '2e-4' --lambda_n2s 2e-2  --lambda_reg 1
+python test_KLAP.py  --save-dir "./experiments/KLAP" --gpu_num 0  --mode quad  --pre_train "PRETRAINED_WEIGHT_PATH" --lr '2e-4' --lambda_n2s 2e-2  --lambda_reg 1
